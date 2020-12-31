fc78dc45e65106bcce81a445607a33654a6de88e
Mohammed Yahya <my.almusaddar@gmail.com>
2020-12-31 15:45:47 +0200

    📦 NEW: add tags

--- accounts.tf
+++ accounts.tf
@@ -6,19 +6,21 @@ resource "aws_organizations_account" "account" {
 
   iam_user_access_to_billing = var.accounts[count.index].allow_iam_users_access_to_billing ? "ALLOW" : "DENY"
 
-  parent_id = [for ou in local.all_ou_attributes: ou.id if ou.name == var.accounts[count.index].organizational_unit][0]
+  parent_id = [for ou in local.all_ou_attributes : ou.id if ou.name == var.accounts[count.index].organizational_unit][0]
+
+  tags = var.tags
 }
 
 locals {
   all_account_attributes = [
     for account in aws_organizations_account.account[*] :
-      {
-        id = account.id,
-        arn = account.arn,
-        name  = account.name
-        email = account.email
-        parent_id = account.parent_id,
-        parent_ou = var.accounts[index(aws_organizations_account.account[*], account)].organizational_unit,
-      }
-    ]
+    {
+      id        = account.id,
+      arn       = account.arn,
+      name      = account.name
+      email     = account.email
+      parent_id = account.parent_id,
+      parent_ou = var.accounts[index(aws_organizations_account.account[*], account)].organizational_unit,
+    }
+  ]
 }
