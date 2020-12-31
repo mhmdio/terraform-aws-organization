8383e70f91d38c59e41865907962f05e97af56de
Mohammed Yahya <my.almusaddar@gmail.com>
2020-12-31 15:59:29 +0200

    👌 IMPROVE

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "feature_set" {
-  type = string
-  default = "ALL"
+  type        = string
+  default     = "ALL"
   description = "The feature set of the organization. One of 'ALL' or 'CONSOLIDATED_BILLING'."
 }
 
@@ -14,17 +14,27 @@ variable "organizational_units" {
       }))
     }))
   }))
-  default = []
+  default     = []
   description = "The tree of organizational units to construct. Defaults to an empty tree."
 }
 
 variable "accounts" {
   type = list(object({
-    name = string,
-    email = string,
-    organizational_unit = string,
+    name                              = string,
+    email                             = string,
+    organizational_unit               = string,
     allow_iam_users_access_to_billing = bool,
   }))
-  default = []
+  default     = []
   description = "The set of accounts to create. Defaults to an empty list."
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags"
+}
+
+variable "aws_service_access_principals" {
+  type        = list(string)
+  description = "List of AWS service principal names for which you want to enable integration with your organization."
 }
